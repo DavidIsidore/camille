831182e5e53a5ed23e9b8ca607c75c95999c86ce
David Isidore <isidoredavid@protonmail.com>
2023-04-18 20:56:36 +0200

    Implémentation du Header

---
+++ src/components/Header/Header.js
@@ -0,0 +1,10 @@
+import React from "react";
+import logo from '../../assets/logo-escale.png'
+
+export default function Header() {
+    return(
+        <div>
+<img src={logo} alt="L'escale de Camille" />
+        </div>
+    )
+}
