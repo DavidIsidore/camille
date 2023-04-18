453cfc0d5c15bd642763e0184640f4a4be14f39e
David Isidore <isidoredavid@protonmail.com>
2023-04-18 21:03:11 +0200

    Mise en place du CSS du header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import logo from '../../assets/logo-escale.png'
+import './header.scss'
 
 export default function Header() {
     return (
-        <div>
-            <img src={logo} alt="L'escale de Camille" />
+        <div className="header">
+            <img src={logo} alt="L'escale de Camille" className="header_logo" />
         </div>
     )
 }
