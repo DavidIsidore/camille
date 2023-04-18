f4a488a5b453fa54e671fb32f53a596e3eb2fec2
David Isidore <isidoredavid@protonmail.com>
2023-04-18 21:25:30 +0200

    Implémentation du CSS de la navbar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,17 +1,38 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { useLocation } from 'react-router-dom'
+import './navbar.scss'
 
 export default function Navbar() {
+    const location = useLocation
     return (
         <nav className="nav">
             <ul className="nav_list">
-                <li>
+                <li
+                    className={
+                        location.pathname === '/'
+                            ? 'nav_list_item_active'
+                            : 'nav_list_item'
+                    }
+                >
                     <Link to="/">Accueil</Link>
                 </li>
-                <li>
+                <li
+                    className={
+                        location.pathname === '/about'
+                            ? 'nav_list_item_active'
+                            : 'nav_list_item'
+                    }
+                >
                     <Link to="/about">À propos</Link>
                 </li>
-                <li>
+                <li
+                    className={
+                        location.pathname === '/contact'
+                            ? 'nav_list_item_active'
+                            : 'nav_list_item'
+                    }
+                >
                     <Link to="/contact">Contact</Link>
                 </li>
             </ul>
